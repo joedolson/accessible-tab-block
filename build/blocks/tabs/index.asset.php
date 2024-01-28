252e639619fdb0adc0aff170a324fced7004dfc2
Joe Dolson <design@joedolson.com>
2024-01-28 17:24:22 -0600

    Build

--- build/blocks/tabs/index.asset.php
+++ build/blocks/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '28769f946151feb708ef');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '398270e91584333cebe8');

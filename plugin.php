c51c4040c00bad571d9673195a5903c6163809ec
Joe Dolson <design@joedolson.com>
2024-11-10 12:26:13 -0600

    Versions

--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@
  * Plugin Name:       Accessible Tabs Block
  * Description:       A custom block to show content in tabpanels.
  * Requires at least: 5.7
- * Requires PHP:      7.0
- * Version:           1.0.2
+ * Requires PHP:      7.4
+ * Version:           1.0.3
  * Author:            Joe Dolson
  * Author URI:        https://www.joedolson.com
  * License:           GPL-2.0-or-later
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: joedolson
 Tags: tabs block, tab block, accessibility
 Requires at least: 5.7
-Tested up to: 6.5
-Stable tag: 1.0.2
-Requires PHP: 5.6
+Tested up to: 6.7
+Stable tag: 1.0.3
+Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 

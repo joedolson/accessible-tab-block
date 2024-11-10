353dc42455fce0d3e0cb92079fd59c8042e76d29
Joe Dolson <design@joedolson.com>
2024-11-10 12:09:37 -0600

    Commit built files

--- build/blocks/tabs/index.asset.php
+++ build/blocks/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5bbb92fbdc2aa3d42453');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a59733d0d27bade13917');
